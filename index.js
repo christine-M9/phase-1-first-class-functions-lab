022f46cef56ad31b78e1c69a4b9b1308f0cf3927
Christine <christinekhekama99@gmail.com>
2023-06-15 17:57:03 +0300

    first two drivers

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+console.log(returnFirstTwoDrivers(drivers)); // return: ["Antonia", "Nuru"]
